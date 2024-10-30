4ec7f7c7ac5c64a43eb211a58af460dfe74098c8
NiloAbraan <145126906+NiloAbraan@users.noreply.github.com>
2024-10-30 10:57:41 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,34 +1,34 @@
-CriaCartao (
+criaCartao (
   'Matematica',
   'Qual o valor de Pi?',
   '3,14'
 )
 
-CriaCartao (
+criaCartao (
   'Geografia',
   'Qual a montanha mais alta do mundo?',
   'Monte Everest'
 )
 
-CriaCartao (
+criaCartao (
   'Fisica',
   'O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
   'Ambos têm o mesmo peso: 1 quilo. Mas, como precisamos de uma porção de algodão maior para compor 1 quilo, temos a sensação de que o mesmo peso de algodão é mais leve do que o chumbo.'
 )
 
-CriaCartao (
+criaCartao (
   'Astronomia',
   'Quais são as fases da Lua?',
   'Nova, crescente, cheia e minguante'
 )
 
-CriaCartao (
+criaCartao (
   'Herois',
   'Kryptonita é a fraqueza de qual super-herói?',
   'Super-Homem'
 )
 
-CriaCartao( 
+criaCartao( 
   'Química',
   'qual o número da molécula de ferro?',
   '26'
